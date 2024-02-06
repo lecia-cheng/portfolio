20deb17fbc34a81c0336bad4a7d88f6b045f8350
lyriao <96245364+lyriao@users.noreply.github.com>
2024-02-06 13:57:16 -0500

    title page

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title">
+        <div className="back-title">DEV</div>
+        <div className="name-title"> &lt; LECIA CHENG &gt; </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
